8739c1e0b33259fbc579e2f2662edf277d178beb
gugw <ggw_hsd@hotmail.com>
2019-08-06 14:45:52 +0800

    update

---
+++ ListViewTest.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace MarketRiskUI
+{
+    partial class ListViewTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSex = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(164, 12);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(530, 204);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.Leave += new System.EventHandler(this.listView1_Leave);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "添加列";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(194, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "添加数据";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(194, 52);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "修改";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(194, 97);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "删除";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtAddress);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.txtPhone);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.txtSex);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(94, 267);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(294, 139);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "姓名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "性别";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "电话";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "籍贯";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(55, 17);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 21);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtSex
+            // 
+            this.txtSex.Location = new System.Drawing.Point(55, 49);
+            this.txtSex.Name = "txtSex";
+            this.txtSex.Size = new System.Drawing.Size(100, 21);
+            this.txtSex.TabIndex = 3;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(55, 78);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 21);
+            this.txtPhone.TabIndex = 3;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(55, 105);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 21);
+            this.txtAddress.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(515, 310);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(515, 340);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "查询";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // ListViewTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "ListViewTest";
+            this.Text = "ListViewTest";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtSex;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+    }
+}
---
+++ ListViewTest.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MarketRiskUI
+{
+    public partial class ListViewTest : Form
+    {
+        public ListViewTest()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListView lv = listView1;
+            //添加列名
+            ColumnHeader c1 = new ColumnHeader();
+            c1.Width = 100;
+            c1.Text = "姓名";
+            ColumnHeader c2 = new ColumnHeader();
+            c2.Width = 100;
+            c2.Text = "性别";
+            ColumnHeader c3 = new ColumnHeader();
+            c3.Width = 100;
+            c3.Text = "电话";
+            //设置属性
+            lv.GridLines = true;  //显示网格线
+            lv.FullRowSelect = true;  //显示全行
+            lv.MultiSelect = false;  //设置只能单选
+            lv.View = View.Details;  //设置显示模式为详细
+            lv.HoverSelection = true;  //当鼠标停留数秒后自动选择
+            //把列名添加到listview中
+            lv.Columns.Add(c1);
+            lv.Columns.Add(c2);
+            lv.Columns.Add(c3);
+            lv.Columns.Add("籍贯", 100);  //相当于上面的添加列名的步骤
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           // this.listView1.BeginUpdate(); //数据更新，UI暂时挂起，直到EndUpdate绘制控件，可以有效避免闪烁并大大提高加载速度
+                                          //获取文本框中的值
+            string name = this.txtName.Text;
+            string sex = this.txtSex.Text;
+            string phone = this.txtPhone.Text;
+            string address = this.txtAddress.Text;
+            //创建行对象
+            ListViewItem li = new ListViewItem(name);
+            //添加同一行的数据
+            li.SubItems.Add(sex);
+            li.SubItems.Add(phone);
+            li.SubItems.Add(address);
+            //将行对象绑定在listview对象中
+            listView1.Items.Add(li);
+            MessageBox.Show("新增数据成功！");
+            
+            //  this.listView1.EndUpdate();  //结束数据处理，UI界面一次性绘制
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+
+                //把修改后的文本框内容添加到listview中
+                listView1.SelectedItems[0].SubItems[0].Text = this.txtName.Text;
+                listView1.SelectedItems[0].SubItems[1].Text = this.txtSex.Text;
+                listView1.SelectedItems[0].SubItems[2].Text = this.txtPhone.Text;
+                listView1.SelectedItems[0].SubItems[3].Text = this.txtAddress.Text;
+                MessageBox.Show("修改数据成功！");
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //当有选择行的数据时
+           
+            if (listView1.Items.Count > 0)
+            {
+                foreach (ListViewItem li in listView1.Items)
+                {
+                        li.BackColor = SystemColors.Window;
+                }
+             
+            }
+
+            if (this.listView1.SelectedItems.Count > 0)
+            {
+
+                //把选择的信息显示在相应的文本框中
+                this.txtName.Text = this.listView1.SelectedItems[0].SubItems[0].Text;
+                this.txtSex.Text = this.listView1.SelectedItems[0].SubItems[1].Text;
+                this.txtPhone.Text = this.listView1.SelectedItems[0].SubItems[2].Text;
+                this.txtAddress.Text = this.listView1.SelectedItems[0].SubItems[3].Text;
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (this.listView1.SelectedItems.Count > 0)
+            {
+                //移除整一行
+                this.listView1.SelectedItems[0].Remove();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //列表有数据
+            if (listView1.Items.Count > 0)
+            {
+                foreach (ListViewItem li in listView1.Items)
+                {
+                    if (li.SubItems[0].Text == this.textBox1.Text)
+                    {
+                        MessageBox.Show("存在该名称");
+                        return;
+                    }
+                }
+                MessageBox.Show("没有找到该姓名");
+            }
+            else
+            {
+                MessageBox.Show("未输入列表数据");
+            }
+        }
+
+        private void listView1_Leave(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                listView1.SelectedItems[0].BackColor = Color.FromArgb(255,0,120,215); //第一个参数为透明度，越大越不透明。
+            }
+        }
+    }
+}
