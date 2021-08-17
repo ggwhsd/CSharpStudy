4429c8d8cd2ce94817680c7cbb6de648c86066c0
ggwhsd <ggw_hsd@hotmail.com>
2021-08-17 20:02:43 +0800

    update

---
+++ OtherProjects/GameBomber/Bomber/GameBuff/DamageBuff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bomber
+{
+    /// <summary>
+    /// 炸弹伤害提升的buff，获取可以提升炸弹的为例
+    /// </summary>
+    class DamageBuff : Objects
+    {
+        public DamageBuff(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+            this.ShowLevel = 0;
+        }
+    }
+}
---
+++ OtherProjects/GameBomber/Bomber/GameBuff/SpeedBuff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bomber
+{
+    /// <summary>
+    /// 加速的buff，获得可以提升移动速度
+    /// </summary>
+    class SpeedBuff:Objects
+    {
+        public SpeedBuff(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+            this.ShowLevel = 0;
+        }
+    }
+}
